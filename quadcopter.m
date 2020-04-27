afa89951d5b8c14cb2166955ef863d67b6fa7ee3
Kemal Mahmuljin <kemal.mahmuljin@student.kuleuven.be>
2020-04-27 18:58:13 +0200

    added Discretization part --not sure about correctness

--- quadcopter.m
+++ quadcopter.m
@@ -1,3 +1,10 @@
+clear all;
+%% Loading references
+load references_02.mat
+n_states = 12;
+n_inputs = 4;
+n_outputs = 6;
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % 1. Linearization
 %       First run this before test_linear_model.slx
@@ -15,6 +22,7 @@ I_yy = 5e-3;    % QC inertia y-axes
 I_zz = 1e-2;    % QC inertia z-axes
 c_m = 1e4;      % Motor constant
 
+
 %% Operating point
 x = 0;
 y = 0;
@@ -29,7 +37,7 @@ v_ss = m * g / ( 4 * k * c_m);  % From set of eqs
 
 %% State-Space matrices
 % system matrix
-A = zeros(12);
+A = zeros(n_states);
 
 A(1,4) = 1;
 A(2,5) = 1;
@@ -44,7 +52,7 @@ A(8,11) = 1;
 A(9,12) = 1;
 
 % input matrix
-B = zeros(12,4);
+B = zeros(n_states,n_inputs);
 
 B(6,1) = k * c_m / m; 
 B(6,2) = k * c_m / m; 
@@ -60,7 +68,7 @@ B(12,3) = b * c_m / I_zz;
 B(12,4) = -b * c_m / I_zz;
 
 % output matrix
-C = zeros(6,12);
+C = zeros(n_outputs,n_states);
 
 C(1,4) = 1;
 C(2,5) = 1;
@@ -72,4 +80,74 @@ C(6,12) = 1;
 % NO feedthrough matrix
 D = zeros(6,4);
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 2. Discretization
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+%% Creating system
+sys= ss(A,B,C,D);
+G = tf(sys);
+%Sampling time
+Ts = 0.05;
+
+%% Trapezoidal Rule : bilinear
+G_DT_bilinear = c2d(G,Ts,'tustin');
+
+%% Zero order Hold
+G_DT_zoh = c2d(G,Ts,'zoh');
+%% Forward Euler
+A_FE = eye( size(A,1) ) + A * Ts;
+B_FE = B * Ts;
+C_FE = C;
+D_FE = D;
+
+G_DT_FE = tf( ss(A_FE,B_FE,C_FE,D_FE,Ts) );
+
+% step and frequency responses
+%{
+%% Step response
+T_sim = 5;
+t_sim_steps = [0:Ts:T_sim];
+step(G,G_DT_bilinear,G_DT_zoh,G_DT_FE,t_sim_steps)
+%step(G,G_DT_bilinear,G_DT_zoh,Gpz_no_gain_correction,t)
+legend show
+
+%% Frequency response
+figure;
+H=bodeplot(G,G_DT_bilinear,G_DT_zoh, G_DT_FE);
+%H=bodeplot(G,G_DT_bilinear,G_DT_zoh,Gpz_no_gain_correction);
+legend show
+grid
+
+pp = getoptions(H);
+pp.YLim{1}=[-25 25];
+setoptions(H,pp)
+%}
+
+%% Performing checks on FE discretisation
+fprintf("========================================================================");
+fprintf(['\n\nThe original continiuous time system itself is unstable (pole = 0). \n', ...
+     'The poles of the original matrix are :\n']);
+disp(eig(A))
+fprintf('\n\nThe poles of the discrete system using FE transformation are: \n');
+disp(eig(A_FE))
+fprintf('\nNotice the same unstable poles.\n');
+fprintf("=======================================================================");
+fprintf('\nNext we check if the discretized system is controllable and observable.');
+fprintf(['\n\nWe can see that the rank of the controllability matrix is the same', ...
+    'as the order of the system: \n']);
+fprintf('\nOrder of the system is : %i \n', n_states);
+fprintf('Rank of controllability matrix is : %i \n', rank(ctrb(A_FE, B_FE)));
+fprintf('Rank of observability matrix is : %i \n', rank(obsv(A_FE, C_FE)));
+fprintf(['\nThe system is thus controllable and not observable', ...
+    'the lack of observability is because we do not measure the angular and lateral velocity vectors.']);
+% This doesn't fully explain it ( rank 8 out of 12, but we drop 6 inputs)
+fprintf('\n(A_FE, B_FE) is stabilizable, because all unstable nodes are controllable.\n');
+% checking which modes are NOT observable using PBH Test
+[V_FE,d_FE] = eig(A_FE);
+result_m_FE = C_FE*V_FE;
+fprintf('\nThe first 3 modes associated with the location are NOT observable and the yaw is also not observable.\n'); 
+fprintf('\nThe system is thus NOT detectable!\n');
+fprintf('\nAs the system is not detectable it is also NOT minimal!\n');
+% Checking transmission zero's of discretized model
+tzero(A_FE, B_FE, C_FE, D_FE)
